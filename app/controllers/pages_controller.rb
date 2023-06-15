a75c814f75b7d1ea938935ebec7b7e558d312141
Ben Malandrinos <bmalandrinos@gmail.com>
2023-06-15 21:33:40 +0200

    added styling navbar, scroll for footer plus styling things

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @collection = Collection.new
+    if user_signed_in?
+      redirect_to collections_path
+    else
+      @collection = Collection.new
+    end
   end
 
   def landing
