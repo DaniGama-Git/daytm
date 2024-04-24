2b7ebefe2b8101eace729a6e85cbe1b1536b16fd
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-04-24 11:07:22 +0200

    fix mailchimp sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,8 +15,8 @@ class PagesController < ApplicationController
 
   def create_mailchimp_member
     gibbon = Gibbon::Request.new(api_key: 'fbb7c1c8ec64a89efa370dd401a386d7-us22')
-    email_address = params[:email_address]
-    if gibbon.lists('6a583e5cf0').members.create(body: { email_address:, status: 'subscribed' })
+    email_address = params[:email]
+    if gibbon.lists('6a583e5cf0').members.create(body: { email_address: , status: 'subscribed' })
       flash[:alert] = 'Successfully subscribed!'
     else
       flash[:alert] = 'Something went wrong. Please try again later or reach out to the Daytm Team directly.'
