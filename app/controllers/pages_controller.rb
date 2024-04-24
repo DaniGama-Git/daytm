c79a6712e497f1a2b51ff576a7d2f27c64cceaed
Liam Strickland <liam.strickland96@gmail.com>
2024-04-24 11:05:18 +0200

    fix navbar buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
   def create_mailchimp_member
     gibbon = Gibbon::Request.new(api_key: 'fbb7c1c8ec64a89efa370dd401a386d7-us22')
     email_address = params[:email_address]
-    if gibbon.lists('6a583e5cf0').members.create(body: { email_address:, status: "subscribed" })
+    if gibbon.lists('6a583e5cf0').members.create(body: { email_address:, status: "subscribed"})
       flash[:alert] = 'Successfully subscribed!'
     else
       flash[:alert] = 'Something went wrong. Please try again later or reach out to the Daytm Team directly.'
