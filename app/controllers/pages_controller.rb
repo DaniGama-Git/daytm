a7e0f1383c7e85139d5211d621fe9e842ea820de
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-05-14 11:22:03 +0200

    set mailchimp env variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
   end
 
   def create_mailchimp_member
-    gibbon = Gibbon::Request.new(api_key: 'fbb7c1c8ec64a89efa370dd401a386d7-us22')
+    gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
     email_address = params[:email]
     if gibbon.lists('6a583e5cf0').members.create(body: { email_address: , status: 'subscribed' })
       flash[:alert] = 'Successfully subscribed!'
